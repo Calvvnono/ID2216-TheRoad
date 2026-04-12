1263ff8727c875e3b50f455c5f78d5423eacfc2e
Quan Zhang <quanzha@kth.se>
2026-04-12 22:05:24 +0200

    Updated globe plugin

--- plugins/withGoogleMapsApiKey.js
+++ plugins/withGoogleMapsApiKey.js
@@ -1,20 +1,24 @@
-const { withAndroidManifest } = require('@expo/config-plugins');
+const { withAndroidManifest } = require('expo/config-plugins');
 
 module.exports = function withGoogleMapsApiKey(config, { apiKey }) {
-  return withAndroidManifest(config, (config) => {
-    const mainApplication = config.modResults.manifest.application[0];
-    if (!mainApplication['meta-data']) {
-      mainApplication['meta-data'] = [];
-    }
-    mainApplication['meta-data'] = mainApplication['meta-data'].filter(
-      (item) => item.$['android:name'] !== 'com.google.android.geo.API_KEY',
+  if (!apiKey) return config;
+  return withAndroidManifest(config, (mod) => {
+    const app = mod.modResults.manifest.application?.[0];
+    if (!app) return mod;
+    if (!app['meta-data']) app['meta-data'] = [];
+    const existing = app['meta-data'].find(
+      (m) => m.$?.['android:name'] === 'com.google.android.geo.API_KEY',
     );
-    mainApplication['meta-data'].push({
-      $: {
-        'android:name': 'com.google.android.geo.API_KEY',
-        'android:value': apiKey,
-      },
-    });
-    return config;
+    if (existing) {
+      existing.$['android:value'] = apiKey;
+    } else {
+      app['meta-data'].push({
+        $: {
+          'android:name': 'com.google.android.geo.API_KEY',
+          'android:value': apiKey,
+        },
+      });
+    }
+    return mod;
   });
 };
